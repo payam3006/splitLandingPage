9e1904f906cff647fe8f3e0b07f37c537dcf05ea
Payam3006 <payam3006@gmail.com>
2024-05-02 02:11:07 +0330

    kalak rashti!

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const q = console.log;
+
+function focusOn(thisDiv) {
+  if (thisDiv.id == "playStation") {
+    document.getElementById("playStation").classList.add("mouseOver");
+    document.getElementById("playStation").classList.remove("mouseOff");
+    document.getElementById("xBox").classList.add("mouseOff");
+    document.getElementById("xBox").classList.add("mouseOver");
+  } else {
+    document.getElementById("xBox").classList.add("mouseOver");
+    document.getElementById("xBox").classList.remove("mouseOff");
+    document.getElementById("playStation").classList.add("mouseOff");
+    document.getElementById("playStation").classList.remove("mouseOver");
+  }
+}
+
+const normallize = (thisDiv) => {
+  q(thisDiv);
+};
